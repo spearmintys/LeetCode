69f7564bdd2287cf27b2db9b2a6846c95d321193
spearmintys <122195701+spearmintys@users.noreply.github.com>
2025-06-09 00:08:11 -0400

    Time: 11 ms (18.39%) | Memory: 19.6 MB (77.62%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        output = 0
-        for num in nums:
-            output ^= num
-        return output
+        #output = 0
+        #for num in nums:
+        #    output ^= num
+        #return output
         #for each number in the array
             #check each number in list sequentially 
         #return single number
+
+
+        #brute force
+        counts = {}
+        #how many time that num appears
+        for num in nums:
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1
+       #what number has 1 count
+        for num in counts:
+            if counts[num] == 1:
+                return num
