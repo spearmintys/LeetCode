903cc273d4b66f603db3afd574fa75c11809f543
spearmintys <122195701+spearmintys@users.noreply.github.com>
2025-06-09 00:27:03 -0400

    Time: 5 ms (94.58%) | Memory: 31.6 MB (53.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        #setup arrays
+        singles = set()
+        #for each value in array    
+        for num in nums:
+            #capture value then compare
+            if num in singles:
+                return True
+            else:
+               #true false return
+                singles.add(num)
+        return False
+
+     
